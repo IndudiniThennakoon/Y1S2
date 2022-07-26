5fc1a25735483dd43eb2a7878127510771466601
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-07-26 14:51:12 +0000

    println Vs print

--- Main.java
+++ Main.java
@@ -4,5 +4,11 @@ class Main {
 		System.out.println("Kurunegala");
 		System.out.println("2000");
 
+		// System.out.println Vs. System.out.print
+
+		System.out.print("Indudini\t");
+		System.out.print("Kurunegala\t");
+		System.out.print("2000");
+
   }
 }
