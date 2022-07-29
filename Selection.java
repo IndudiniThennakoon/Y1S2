bf48768f32332d89ac31bdadec1a41957ec8cc9d
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-07-29 18:11:22 +0000

    selection switch

--- Selection.java
+++ Selection.java
@@ -1,24 +1,50 @@
 class Main {
   public static void main(String[] args) {
 
-		System.out.println("Enter 1 -5 any number: ");
+
+		// if -else 
+
+		
+		// System.out.println("Enter 1 -5 any number: ");
+		// Scanner myscanner = new Scanner(System.in);
+
+		// int opt = myscanner.nextInt();
+
+		// if(opt == 1)
+		// {
+		// 	System.out.println("Malabe campus");
+		// }else if( opt ==2){
+		// 	System.out.println("Metro Campus");
+		// }else if(opt == 3){
+		// 	System.out.println("Matara campus");
+		// }else if( opt ==4){
+		// 	System.out.println("Kandy Campus");
+		// }else if(opt == 5){
+		// 	System.out.println("Kurunegala campus");
+		// }else{
+		// 	System.out.println("Invalid option");
+		// }
+
+		// switch
+
+		System.out.println("Enter 1 -3 any number: ");
 		Scanner myscanner = new Scanner(System.in);
 
 		int opt = myscanner.nextInt();
 
-		if(opt == 1)
-		{
-			System.out.println("Malabe campus");
-		}else if( opt ==2){
-			System.out.println("Metro Campus");
-		}else if(opt == 3){
-			System.out.println("Matara campus");
-		}else if( opt ==4){
-			System.out.println("Kandy Campus");
-		}else if(opt == 5){
-			System.out.println("Kurunegala campus");
-		}else{
-			System.out.println("Invalid option");
+		switch (opt){
+			case 1 : 
+				System.out.println("Malabe campus");
+				break;	
+			case 2 :
+				System.out.println("Metro Campus");
+				break;	
+			case 3 :
+				System.out.println("Matara campus");
+				break;
+			default:
+				System.out.println("Invalid option");
+				
 		}
 	}
 
