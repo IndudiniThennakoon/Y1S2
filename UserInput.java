ab402802343c85e12a27915ee71c18d49b6df7e5
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-07-29 14:18:39 +0000

    Basic way to find the largest number of three numbers

--- UserInput.java
+++ UserInput.java
@@ -32,12 +32,22 @@ class UserInput{
 		if((no1 < no2) && (no1 < no3)){
 			min = no1;
 		}else if((no2 < no3) && (no2 < no1)){
-			min = no3;
+			min = no2;
 		}else{
 			min =no3;
 		}
 
+		int max;
+		
+		if((no1 > no2) && (no1 > no3)){
+			max = no1;
+		}else if((no2 > no3) && (no2 > no1)){
+			max = no2;
+		}else{
+			max =no3;
+		}
 		System.out.println("The smallest = " + min);
-	
+		System.out.println("The largest = " + max);
+		
 	}
 }
