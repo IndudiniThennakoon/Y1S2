784338de73c6331021cda73c7a821609f070dcec
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-07-29 18:05:20 +0000

    slection if-else

---
+++ Selection.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+
+		System.out.println("5 . Kurunegala");
+		System.out.println("6 . Jaffna");
+		System.out.println("0 . Exit");
+
+		System.out.println("Enter 1 -5 any number: ");
+		Scanner myscanner = new Scanner(System.in);
+
+		int opt = myscanner.nextInt();
+
+		if(opt == 1)
+		{
+			System.out.println("Malabe campus");
+		}else if( opt ==2){
+			System.out.println("Metro Campus");
+		}else if(opt == 3){
+			System.out.println("Matara campus");
+		}else if( opt ==4){
+			System.out.println("Kandy Campus");
+		}else if(opt == 5){
+			System.out.println("Kurunegala campus");
+		}else{
+			System.out.println("Invalid option");
+		}
+	}
+
+}
