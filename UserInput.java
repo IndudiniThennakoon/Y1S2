86f2ab7ca32adaaadc579411d2305387a9c9a7a9
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-07-29 14:15:18 +0000

    Basic way to find the smallest number of three numbers

--- UserInput.java
+++ UserInput.java
@@ -6,18 +6,38 @@ class UserInput{
 
 		// EX 01
 		
-		System.out.println("Enter length: ");
+		// System.out.println("Enter length: ");
+		// Scanner myscanner = new Scanner(System.in);
+		// int length = myscanner.nextInt();
+
+		// System.out.println("Enter width: ");
+		// int width = myscanner.nextInt();
+
+		// int perimeter = 2 * ( length + width );
+
+		// System.out.println("Perimeter: " + perimeter);
+
+		//EX 02
+
 		Scanner myscanner = new Scanner(System.in);
-		int length = myscanner.nextInt();
+		System.out.println("Enter number 1 = ");
+		int no1 = myscanner.nextInt();
+		System.out.println("Enter number 2 = ");
+		int no2 = myscanner.nextInt();
+		System.out.println("Enter number 2 = ");
+		int no3 = myscanner.nextInt();
 
-		System.out.println("Enter width: ");
-		int width = myscanner.nextInt();
+		int min;
 
-		int perimeter = 2 * ( length + width );
+		if((no1 < no2) && (no1 < no3)){
+			min = no1;
+		}else if((no2 < no3) && (no2 < no1)){
+			min = no3;
+		}else{
+			min =no3;
+		}
 
-		System.out.println("Perimeter: " + perimeter);
-
-		
+		System.out.println("The smallest = " + min);
 	
 	}
 }
