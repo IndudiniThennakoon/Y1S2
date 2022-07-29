92a8f5ca9e7a1eedd229f92a445aa6277f5439a1
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-07-29 14:21:03 +0000

    finding the largest and the smallest number  of theer numbers using for loop and array

--- UserInput.java
+++ UserInput.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
 
 class UserInput{
 	public static void main(String[] args) {
@@ -19,35 +19,62 @@ class UserInput{
 
 		//EX 02
 
+		// Scanner myscanner = new Scanner(System.in);
+		// System.out.println("Enter number 1 = ");
+		// int no1 = myscanner.nextInt();
+		// System.out.println("Enter number 2 = ");
+		// int no2 = myscanner.nextInt();
+		// System.out.println("Enter number 2 = ");
+		// int no3 = myscanner.nextInt();
+
+		// int min;
+
+		// if((no1 < no2) && (no1 < no3)){
+		// 	min = no1;
+		// }else if((no2 < no3) && (no2 < no1)){
+		// 	min = no2;
+		// }else{
+		// 	min =no3;
+		// }
+
+		// int max;
+		
+		// if((no1 > no2) && (no1 > no3)){
+		// 	max = no1;
+		// }else if((no2 > no3) && (no2 > no1)){
+		// 	max = no2;
+		// }else{
+		// 	max =no3;
+		// }
+		// System.out.println("The smallest = " + min);
+		// System.out.println("The largest = " + max);
+
+
+		//EX 02 - using for loop
+
+		int arr[] = new int[3];
+		int max = 0;
+		int min = 0;
 		Scanner myscanner = new Scanner(System.in);
-		System.out.println("Enter number 1 = ");
-		int no1 = myscanner.nextInt();
-		System.out.println("Enter number 2 = ");
-		int no2 = myscanner.nextInt();
-		System.out.println("Enter number 2 = ");
-		int no3 = myscanner.nextInt();
-
-		int min;
-
-		if((no1 < no2) && (no1 < no3)){
-			min = no1;
-		}else if((no2 < no3) && (no2 < no1)){
-			min = no2;
-		}else{
-			min =no3;
+		for (int i = 0; i <3; i ++)
+		{
+			System.out.print("Enter Integer: ");
+			arr[i] = myscanner.nextInt();
+		}
+	
+		for(int k = 0 ; k <3 ; k++){
+			if(max < arr[k]){ 
+				max = arr[k];
 		}
 
-		int max;
-		
-		if((no1 > no2) && (no1 > no3)){
-			max = no1;
-		}else if((no2 > no3) && (no2 > no1)){
-			max = no2;
-		}else{
-			max =no3;
+		min = arr[0]; 
+
+		for(int l = 0 ; l<3 ; l++){
+			if(min > arr[l]){ 
+				min = arr[l]; 
+			}
 		}
-		System.out.println("The smallest = " + min);
-		System.out.println("The largest = " + max);
-		
+		System.out.println("Max: " + max);
+		System.out.println("Min: " + min);
 	}
 }
