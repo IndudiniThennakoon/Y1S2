394222c3412db96a8b078b3e4b5fc85255e3046d
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-07-31 16:59:56 +0000

    #commit: hospital management system

---
+++ week02_lec02/Ex01.java
@@ -0,0 +1,20 @@
+//  Hospital managment system
+
+class Receptionist{
+
+	private string ID;
+	private string name;
+	private int age;
+	private string address;
+	private int workingHours;
+	
+}
+
+
+
+public class Ex01{
+
+	public static void main(String[] args){
+		
+	}
+}
