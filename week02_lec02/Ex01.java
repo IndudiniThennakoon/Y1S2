f346a1428a59f83409cd0a3ab054d814d84399e9
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-07-31 17:01:04 +0000

    #commit: added getters and setters

--- week02_lec02/Ex01.java
+++ week02_lec02/Ex01.java
@@ -10,7 +10,45 @@ class Receptionist{
 	
 }
 
+public void setID(String ID){
+	this.ID = ID;
+}
 
+public Strong getID(){
+	return ID;
+}
+
+public void setName(String name){
+	this.name = name;
+}
+
+public String getName(){
+	return name;
+}
+
+public void setAge(int age){
+	this.age = age;
+}
+
+public int getAge(){
+	retrun age;
+}
+
+public void setAddress(String address){
+	this.address = address;
+}
+
+public String getAddress(){
+	return address;
+}
+
+public void setWorkingHours(int workingHours){
+	this.workingHours = workingHours;
+}
+
+public int getWorkingHours(){
+	return workingHours;
+}
 
 public class Ex01{
 
