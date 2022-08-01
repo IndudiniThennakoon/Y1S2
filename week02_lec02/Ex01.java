0730feeab82cca4cd1cf78631f5e94effcedb87c
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-01 17:20:22 +0000

    #commit: fixed bugs

--- week02_lec02/Ex01.java
+++ week02_lec02/Ex01.java
@@ -4,24 +4,23 @@ class Room{
 	private int roomNo;
 	private boolean isAvailable;
 	
-}
-
-// Setters and getters for Room class
-
-public void setRoomNo(int roomNo) {
-	this.roomNo = roomNo;
-}
-
-public int getRoomNo(){
-	return roomNo;
-}
-
-public boolean isAvailable(){
-	return isAvailble;
-}
-
-public void setAvailble(boolean isAvailable){
-	this.isAvailable = isAvailable;
+	// Setters and getters for Room class
+	
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+	
+	public int getRoomNo(){
+		return roomNo;
+	}
+	
+	public boolean isAvailable(){
+		return isAvailble;
+	}
+	
+	public void setAvailble(boolean isAvailable){
+		this.isAvailable = isAvailable;
+	}
 }
 
 class Receptionist{
@@ -31,61 +30,59 @@ class Receptionist{
 	private int age;
 	private string address;
 	private int workingHours;
+	private Room room;
 	
-}
-
 // implement the method
-
-
-public boolean checkRoomavailblity(int roomNo){
+	public boolean checkRoomavailblity(Room room){
+		this.room = room;
+		return room.isAvailable();
+	}
 	
-	if()
-}
-
-public boolean isAvailable(){
-	return true;
-}
+	public boolean isAvailable(){
+		return true;
+	}
 
 
-// setters and getters
-public void setID(String ID){
-	this.ID = ID;
-}
+	// setters and getters
+	public void setID(String ID){
+		this.ID = ID;
+	}
+	
+	public Strong getID(){
+		return ID;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setAge(int age){
+		this.age = age;
+	}
+	
+	public int getAge(){
+		retrun age;
+	}
+	
+	public void setAddress(String address){
+		this.address = address;
+	}
 
-public Strong getID(){
-	return ID;
-}
-
-public void setName(String name){
-	this.name = name;
-}
-
-public String getName(){
-	return name;
-}
-
-public void setAge(int age){
-	this.age = age;
-}
-
-public int getAge(){
-	retrun age;
-}
-
-public void setAddress(String address){
-	this.address = address;
-}
-
-public String getAddress(){
-	return address;
-}
-
-public void setWorkingHours(int workingHours){
-	this.workingHours = workingHours;
-}
-
-public int getWorkingHours(){
-	return workingHours;
+	public String getAddress(){
+		return address;
+	}
+	
+	public void setWorkingHours(int workingHours){
+		this.workingHours = workingHours;
+	}
+	
+	public int getWorkingHours(){
+		return workingHours;
+	}	
 }
 
 public class Ex01{
