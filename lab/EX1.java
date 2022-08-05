9c1cb99586258bf05495573e8ff216b6f26ea4c8
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-08-06 05:07:17 +0530

    #commit: ex1

---
+++ lab/EX1.java
@@ -0,0 +1,9 @@
+class Ex01{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+        System.out.println("It's been ince knowing you");
+        System.out.println("Goodbye world!");   
+    }
+}
