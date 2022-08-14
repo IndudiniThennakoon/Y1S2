04eecfb90ca676d7c701607fb37fc5fe26244182
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-14 07:43:54 +0000

    #commit: tutorial1

---
+++ OOP/week03_lec02/Weekend/Tutorial01/Student.java
@@ -0,0 +1,8 @@
+class Student{
+	private float marks1;
+	private float marks2;
+	private float marks3;
+
+	
+}
+
---
+++ OOP/week03_lec02/Weekend/Tutorial01/TuteMain.java
@@ -0,0 +1,7 @@
+public class TuteMain {
+	
+	public static void main (String[] args) {
+		
+
+
+}
