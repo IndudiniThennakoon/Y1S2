dec5e6c9d2d4ac4af8be4d04ff06c2457eb14c1f
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-14 07:08:49 +0000

    #commit: add default and overloaded const. for Sub class called Student

--- OOP/week03_lec02/inheritance/Student.java
+++ OOP/week03_lec02/inheritance/Student.java
@@ -2,4 +2,11 @@ public class Student extends Person {
 	private String sid;
 	private double fee;
 
+	//	default con
+	
+	public Student() {
+		System.out.println("Student Defult");
+	}
+//	over con
+	public Student(String NIC, String name,String sid, double fee) {
 }
