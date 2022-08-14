1ab9dca30ee813435413d00cc50261cace2c029d
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-14 05:45:05 +0000

    #commit: default constructor

--- OOP/week03_lec02/Receptionist.java
+++ OOP/week03_lec02/Receptionist.java
@@ -3,6 +3,12 @@ public class Receptionist {
 	private String name;
 	private double salary;
 	
+	//	default constructor
+	public Receptionist() {
+		id = 0;
+		name = null;
+		salary = 0.0; 
+	}
 
 	
 	public  boolean checkRoomAvailability( int roomNo) {
