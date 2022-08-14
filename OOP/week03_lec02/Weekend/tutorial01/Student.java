ddb698015bb147addeb29a09031fe668d65914e0
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-14 07:49:56 +0000

    #commit: Exercise 1 part c

--- OOP/week03_lec02/Weekend/tutorial01/Student.java
+++ OOP/week03_lec02/Weekend/tutorial01/Student.java
@@ -10,10 +10,25 @@ class Student{
 		this.marks2 = marks2;
 		this.marks3 = marks3;
 	}
-
+// (1) (c)
+	public Student(int id, String name, double marks1, double marks2, double marks3) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.marks1 = marks1;
+		this.marks2 = marks2;
+		this.marks3 = marks3;
+	}
+// (1) (b)
 	public void calAvg() {
 		this.avg = (this.marks1 + this.marks2 + this.marks3)/3.0;
 	}
-	
+
+	//  (1) (c)
+	public void displayDetails() {
+		System.out.println("ID: " + this.id );
+		System.out.println("Name: " + this.name );
+		System.out.println("Average: " + this.avg );
+	}
 }
 
