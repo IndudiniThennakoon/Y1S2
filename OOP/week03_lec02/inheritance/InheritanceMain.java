02e2e51af763e34429cffe251e0ab14fc690317d
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-14 07:10:28 +0000

    #commit: print person overloaded ocnst.

--- OOP/week03_lec02/inheritance/InheritanceMain.java
+++ OOP/week03_lec02/inheritance/InheritanceMain.java
@@ -5,6 +5,10 @@ public class InheritanceMain {
 		Person ob1 = new Person(); 
 		
 //		belongs to person class default cons
+
+		Person ob2 = new Person("25252536V", "Indu"); 
 		
+//		belongs to person class overloaded cons
+	
 	}
 }
