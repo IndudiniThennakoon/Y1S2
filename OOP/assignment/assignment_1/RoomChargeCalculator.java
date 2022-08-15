52f08e3740cf7a37ac475f43bad625ea299c0b0b
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-15 11:35:22 +0000

    #commit: added getChargeDeluxDouble method

--- OOP/assignment/assignment_1/RoomChargeCalculator.java
+++ OOP/assignment/assignment_1/RoomChargeCalculator.java
@@ -16,4 +16,31 @@ class RoomChargeCalculator {
 		this.wifi = wifi;
 	}
 
+	public void getChargeDeluxDouble(boolean balcony,boolean parking,boolean tv,boolean kitchen,boolean wifi) {
+		
+		 fixedFee = 5000;
+		 
+		 
+		 if(balcony) {
+			 this.balcony = 500;
+		 }
+		 
+		 if (parking) {
+			this.parking = 200;	
+		 }
+		 
+		 if(tv) {
+			this.tv = 200;	
+		 }
+
+		 if (kitchen) {
+			 this.kitchen = 1000; 
+		 }
+
+		 if (wifi) {
+			 this.wifi = 100; 
+		 }else {
+			 System.out.println("\nNo additional charges");
+		 }
+	}
 }
