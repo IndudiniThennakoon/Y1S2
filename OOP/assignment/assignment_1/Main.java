c85ebe5ea1120785cd934c44762daa8c9e826cfb
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-15 11:40:35 +0000

    #commit:  print room details

--- OOP/assignment/assignment_1/Main.java
+++ OOP/assignment/assignment_1/Main.java
@@ -4,4 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		RoomChargeCalculator rm = new RoomChargeCalculator();
+
+		System.out.println("Select the Room Category\n");
+
+			System.out.println("1 - Delux-Double\n2 - Standard-Family\n3 - Standard-Single\n4 - Quit");
 }
