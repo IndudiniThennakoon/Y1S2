5995bbb1d7f515ac34fff3294d2b44b82f637ee1
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-15 11:41:41 +0000

    #commit: input the room category

--- OOP/assignment/assignment_1/Main.java
+++ OOP/assignment/assignment_1/Main.java
@@ -8,4 +8,11 @@ public class Main {
 		System.out.println("Select the Room Category\n");
 
 			System.out.println("1 - Delux-Double\n2 - Standard-Family\n3 - Standard-Single\n4 - Quit");
+
+		System.out.println("Select the Room Category\n");
+
+			System.out.println("1 - Delux-Double\n2 - Standard-Family\n3 - Standard-Single\n4 - Quit");
+
+			Scanner myscanner = new Scanner(System.in);
+			int category = myscanner.nextInt()
 }
