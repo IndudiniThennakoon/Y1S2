0aeb2be49708242064300b803849426d77f1d64f
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-08-15 11:32:00 +0000

    #commit : variables added

---
+++ OOP/assignment/assignment_1/RoomChargeCalculator.java
@@ -0,0 +1,10 @@
+class RoomChargeCalculator {
+
+	private double fixedFee;
+	private double balcony;
+	private double parking;
+	private double tv;
+	private double kitchen;
+	private double wifi;
+
+}
