71f6342836242753c778e6a0aa86eeb67b7c1b66
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-08-17 07:38:55 +0530

    calculate the max of 25 values

--- OOP/assignment/assignment_1/ex02/RandNum.java
+++ OOP/assignment/assignment_1/ex02/RandNum.java
@@ -27,5 +27,13 @@ class RandNum {
 
         int maxVal = arr[0][0];
 
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > maxVal) {
+                    maxVal = arr[i][j];
+                }
+            }
+        }
+
     }
 }
