3543003bb87da3a22a8381875282f37e915d54d9
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-08-17 07:32:52 +0530

    default constructor added

--- OOP/assignment/assignment_1/ex02/RandNum.java
+++ OOP/assignment/assignment_1/ex02/RandNum.java
@@ -1,4 +1,14 @@
 class RandNum {
+    // create a 2D array
     int[][] arr = new int[5][5];
+
+    int max = 100;
+    int min = 1;
+
+    // default constructor
+    public RandNum() {
+    }
+
+
 
 }
