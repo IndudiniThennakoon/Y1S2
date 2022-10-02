5ed7e3080679d4d8dcada5ed9f51bb9d7e081a1f
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-02 10:09:26 +0530

    commit

---
+++ OOP/lab/lab1/EX1.java
@@ -0,0 +1,15 @@
+package OOP.lab;
+
+//Exercise 1 (Time: 5 minutes)
+//        Write a program that produces the following output:
+//        Hello World!
+//        It's been nice knowing you.
+//        Goodbye world!
+
+public class EX1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("It's been ince knowing you");
+        System.out.println("Goodbye world!");
+    }
+}
