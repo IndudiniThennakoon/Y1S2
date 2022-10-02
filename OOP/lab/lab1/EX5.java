1b97c62c080bb6090ec93a5958ea1c8e94d8524e
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-02 15:39:01 +0530

    Ex05 using for loop

--- OOP/lab/lab1/EX5.java
+++ OOP/lab/lab1/EX5.java
@@ -12,15 +12,26 @@ package OOP.lab;
 
 public class EX5 {
     public static void main(String[] args) {
-       int i = 1;
-       while(i <= 5){
-           int j = 1;
-           while(j <=5){
-               System.out.print("* ");
-               j++;
-           }
-           System.out.println();
-            i++;
+//       int i = 1;
+//       while(i <= 5){
+//           int j = 1;
+//           while(j <=5){
+//               System.out.print("* ");
+//               j++;
+//           }
+//           System.out.println();
+//            i++;
+//        }
+//    }
+
+        for (int i = 0; i <=4; i++){
+            for(int  j= 4; j < 5; j++ ){
+                System.out.print("  ");
+            }
+            for (int j=1; j<=(2*i-1); j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
         }
     }
 }
