77e0b2ec9535d5f4a0841f56e82989e549abbac9
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-02 09:52:13 +0530

    Ex 02

---
+++ OOP/lab/lab1/EX2.java
@@ -0,0 +1,17 @@
+package OOP.lab;
+
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+import java.sql.SQLOutput;
+
+public class EX2 {
+    public static void main(String[] args) {
+        System.out.println("=============================");
+        System.out.println("=\tStudent Information\t\t\t=");
+        System.out.println("=============================");
+        System.out.println("= Name\t\t: ARDP Ranasignhe\t=");
+        System.out.println("= Reg No\t: DIS\08MA1234\t\t=");
+        System.out.println("= Address\t: Malabe\t\t\t=");
+        System.out.println("=============================");
+    }
+}
