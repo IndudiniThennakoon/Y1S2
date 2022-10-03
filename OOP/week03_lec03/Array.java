0396ef499e83968d5643834b060b988d2371206a
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-03 11:29:01 +0530

    creating array in way2

--- OOP/week03_lec03/Array.java
+++ OOP/week03_lec03/Array.java
@@ -2,7 +2,9 @@ public class Array {
     public static void main(String[] args) {
     // creating array in way1
         int arr1[] = new int[3];
-        creating array in way1
 
+     // creating array in way2
+        int arr2[];
+        arr2 = new int[3];
     }
 }
