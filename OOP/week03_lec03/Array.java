2a4a36f4efed66829f41275a6043769d1b9cb15c
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-03 11:31:54 +0530

    assigning values to the array indexes

--- OOP/week03_lec03/Array.java
+++ OOP/week03_lec03/Array.java
@@ -1,10 +1,23 @@
 public class Array {
     public static void main(String[] args) {
-    // creating array in way1
+        // creating array in way1
         int arr1[] = new int[3];
 
-     // creating array in way2
+        // creating array in way2
         int arr2[];
         arr2 = new int[3];
+
+        // assigning values to the array indexes
+        int arr1[5];
+        int arr1[63];
+        int arr1[9];
+
+
+        int arr2[8];
+        int arr2[7];
+        int arr2[55];
+
+
+
     }
 }
