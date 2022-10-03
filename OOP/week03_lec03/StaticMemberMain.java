10133ad9a3f8a9fb9ca32221e08163a8a3492ecd
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-03 13:37:42 +0530

    local class static variables

--- OOP/week03_lec03/StaticMemberMain.java
+++ OOP/week03_lec03/StaticMemberMain.java
@@ -2,6 +2,15 @@ public class StaticMemberMain {
     public static void main(String[] args) {
 
 
+        // there are 3 type of variables - local, class, static
+        // local - belongs to a particular block
+        // class - valiable which is defineing a class- in this example name and age
+        // class variables are belongs to a partiular object. whenever we creating an objects class variables are repeated
+        // static - belong to the class. there is no copy
+        // static varibles are donot belongs to an indivial objects. common to all the instances
+
+
+
     }
 
 }
