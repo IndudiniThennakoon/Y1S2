7787ad20d628aac4d19ad02c3e7e79f84ee1c28c
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-03 14:17:23 +0530

    passing values to show method

--- OOP/week03_lec03/PassingObjectAsParaneter.java
+++ OOP/week03_lec03/PassingObjectAsParaneter.java
@@ -1,3 +1,15 @@
 public class PassingObjectAsParaneter {
 
+    String name;
+    int age;
+
+    public void show(){
+        System.out.println("Name is: " + this.name);
+    }
+
+    public void show(int x){
+        System.out.println("number : " +x);
+
+    }
 }
+
