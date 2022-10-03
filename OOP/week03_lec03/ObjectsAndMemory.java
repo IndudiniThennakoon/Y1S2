f2098acb35450b1e544109bfb4f10e8f79be669b
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-03 12:57:02 +0530

    memory allocation of reference variable

--- OOP/week03_lec03/ObjectsAndMemory.java
+++ OOP/week03_lec03/ObjectsAndMemory.java
@@ -1,3 +1,13 @@
 public class ObjectsAndMemory {
+    public static void main(String[] args) {
+        ObjectMemoryStudent ob = new ObjectMemoryStudent;
+        // when we type the new key word only object create. ob is the reference variable
+        // reference variable hold the memory address of ob object
+        // If we print the value of the reference variable of ob it will print newly created objects' (ob objects' ) memory address
+
+        System.out.println(ob);
+
+
+    }
 
 }
