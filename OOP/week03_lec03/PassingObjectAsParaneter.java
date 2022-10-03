52c640fc28392af9693902b1b65e26c422457845
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-03 14:22:17 +0530

    print name

--- OOP/week03_lec03/PassingObjectAsParaneter.java
+++ OOP/week03_lec03/PassingObjectAsParaneter.java
@@ -2,18 +2,19 @@ public class PassingObjectAsParaneter {
 
     String name;
     int age;
-
-    public void show(){
+    public void show() {
         System.out.println("Name is: " + this.name);
     }
-
     public void show(int x){
         System.out.println("number : " +x);
 
     }
-
     public void show(PassingObjectAsParameter ref){
         System.out.println("number : " + x); // print x
+
+    }
+    public void show(PassingObjectAsParameter ref){
+        System.out.println("number : " + ref.name); // print name
 
     }
 }
