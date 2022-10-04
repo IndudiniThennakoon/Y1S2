b0e26a9d02fb373ef5eb354a1bea71eaf722db86
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-05 03:54:05 +0530

    MathopThrowExample

---
+++ OOP/week05_lec04/src/MathOpThorwMain.java
@@ -0,0 +1,7 @@
+public class MathOpThorwMain {
+    public static void main(String[] args) {
+        MathOpThrowExample math = new MathOpThrowExample();
+        math.add(2,0);
+        math.divide(2,0);
+    }
+}
