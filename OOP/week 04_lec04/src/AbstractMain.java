7cc819ad73503c2976011f1a4ba5f0be468fb366
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-04 16:20:14 +0530

    create objects from Rectangle class

--- OOP/week 04_lec04/src/AbstractMain.java
+++ OOP/week 04_lec04/src/AbstractMain.java
@@ -3,6 +3,9 @@ public class AbstractMain {
         //Shape ob = new Shape(); -wrong (getting error)
         // here we got an error because we cannot create instances(objects) from an abstract class
 
+        Rectangle ob =new Rectangle();
+        ob.getArea();
+
     }
 
 }
