70ef82eafaeb60f887e9ac4d5c527b37d5e72b6b
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-04 15:53:36 +0530

    abstract method

--- OOP/week 04_lec04/src/Shape.java
+++ OOP/week 04_lec04/src/Shape.java
@@ -3,5 +3,18 @@ public class Shape {
 
     }
 
+    // we can not define area for a shape because there is no any function to
+    // to calculate the area of a class. So in here getArea(
+    // method does not have a return type.
+
+    // In java if we do nit find a particular implementation
+    //  but still we want to keep that in the super class
+    //  then the best option is defined as abstracts.
+
+    // Abstract methods do not have a method body. Not even
+    // opening or ending curly brackets.
+
+    abstract public int getArea(); // if we want we can add parameters also
+
 
 }
