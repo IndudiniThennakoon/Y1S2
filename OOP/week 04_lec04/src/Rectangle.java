7a1d999610b51415079ba88ca23254c8caa46fc7
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-04 16:18:16 +0530

    fixed cannot create abstract classes objects

--- OOP/week 04_lec04/src/Rectangle.java
+++ OOP/week 04_lec04/src/Rectangle.java
@@ -1,6 +1,14 @@
-public abstract class Rectangle extends Shape {
+public class Rectangle extends Shape {
     // child class MUST override all the abstract methods of thr parent class
     // If a child class did not override all the abstract method of the parent, then the child class also abstract
 
+    // since we cannot create objects from abstract class, we can remove the abstract keyword
+    // and the override the all the abstract methods of the parent class
+
+    private int height;
+    private int width;
+    public int getArea(){
+        return this.height * this.width;
+    }
 
 }
