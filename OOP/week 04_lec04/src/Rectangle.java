16e39a396aa515af5ef28b4882853892e6934245
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-04 16:06:58 +0530

    abstract child class

--- OOP/week 04_lec04/src/Rectangle.java
+++ OOP/week 04_lec04/src/Rectangle.java
@@ -1,5 +1,6 @@
-public class Rectangle extends Shape {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+public abstract class Rectangle extends Shape {
+    // child class MUST override all the abstract methods of thr parent class
+    // If a child class did not override all the abstract method of the parent, then the child class also abstract
+
+
 }
