ec5844443439380a3c251a1f20e7be8e7296e928
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-04 16:14:11 +0530

    error

--- OOP/week 04_lec04/src/AbstractMain.java
+++ OOP/week 04_lec04/src/AbstractMain.java
@@ -1,6 +1,6 @@
 public class AbstractMain {
     public static void main(String[] args) {
-        Shape ob = new Shape();
+        //Shape ob = new Shape(); -wrong (getting error)
         // here we got an error because we cannot create instances(objects) from an abstract class
 
     }
