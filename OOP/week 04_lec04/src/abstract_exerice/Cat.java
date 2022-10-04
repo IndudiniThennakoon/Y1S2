831d20883f07c7c3e766fa94df37eea14b6f599a
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-04 16:46:19 +0530

    implement abstract methods in subclasses

--- OOP/week 04_lec04/src/abstract_exerice/Cat.java
+++ OOP/week 04_lec04/src/abstract_exerice/Cat.java
@@ -1,4 +1,13 @@
 package abstract_exerice;
 
-public class Cat {
+public class Cat extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("I am eating rats");
+    }
+    @Override
+    public void makeSounds() {
+        System.out.println("Meaw Meaw");
+
+    }
 }
