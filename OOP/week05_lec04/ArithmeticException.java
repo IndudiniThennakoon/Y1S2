979b9ae5d4694c4dec4d0a7825244fd6ec89eb06
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-05 03:38:48 +0530

    delete unwanted file

--- OOP/week05_lec04/ArithmeticException.java
+++
@@ -1,7 +0,0 @@
-public class ArithmeticException {
-    public static void main(String[] args) {
-        int num1 = 0, num2 =10,div;
-        div= num2 / num1; // we cannot divid num2 in num1 (zero)
-
-    }
-}
