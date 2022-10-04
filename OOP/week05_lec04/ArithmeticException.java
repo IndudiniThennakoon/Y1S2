336f6c84a90a8ddfd80aad7c5cdcb493f0e8e880
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-05 02:52:13 +0530

    ArithmeticException- identify the problem

--- OOP/week05_lec04/ArithmeticException.java
+++ OOP/week05_lec04/ArithmeticException.java
@@ -1,6 +1,7 @@
 public class ArithmeticException {
     public static void main(String[] args) {
         int num1 = 0, num2 =10,div;
-        div= num2 / num1;
+        div= num2 / num1; // we cannot divid num2 in num1 (zero)
+
     }
 }
