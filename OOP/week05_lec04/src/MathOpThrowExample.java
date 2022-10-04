fb1df1817fe955c1d8f13c313d345aafe7e9e291
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-10-05 04:11:48 +0530

    throw example

--- OOP/week05_lec04/src/MathOpThrowExample.java
+++ OOP/week05_lec04/src/MathOpThrowExample.java
@@ -2,7 +2,7 @@ public class MathOpThrowExample {
     public int add(int a, int b) {
         return a+b;
     }
-    public double divide(int a, int b) {
+    public double divide(int a, int b) throws ArithmeticException {
         return a / b;
     }
 }
