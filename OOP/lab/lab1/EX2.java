679c90310630cd9ff89e4062155851158477d87f
Indudini Thennakoon <99181964+IndudiniThennakoon@users.noreply.github.com>
2022-10-26 07:18:12 +0530

    fixed bugs

--- OOP/lab/lab1/EX2.java
+++ OOP/lab/lab1/EX2.java
@@ -17,7 +17,7 @@ public class EX2 {
         System.out.println("=\tStudent Information\t\t\t=");
         System.out.println("=============================");
         System.out.println("= Name\t\t: ARDP Ranasignhe\t=");
-        System.out.println("= Reg No\t: DIS\08MA1234\t\t=");
+        System.out.println("= Reg No\t: DIS\\08\\MA\\1234\t=");
         System.out.println("= Address\t: Malabe\t\t\t=");
         System.out.println("=============================");
     }
